67bc1a53192a9205c2bf67ad36bd1758b25fba71
jaber1 <j.bergmannfonseca@s-v.de>
2019-12-27 15:22:04 +0100

    init commit

---
+++ src/views/Mixer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mixer">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Mixer',
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MusicList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="music-list">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MusicList',
+}
+</script>
+
+<style>
+
+</style>
